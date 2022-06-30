e868309f41349a2f744ab155d28b6945fc343b7c
Morgan Lesko <15270653+rockhopsoft@users.noreply.github.com>
2022-06-30 12:25:26 -0400

    Update RockHopSoftComServiceProvider.php

--- src/RockHopSoftComServiceProvider.php
+++ src/RockHopSoftComServiceProvider.php
@@ -12,20 +12,17 @@ class RockHopSoftComServiceProvider extends ServiceProvider
             return new RockHopSoftComFacade();
         });
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
-        $dbMig = '2020_09_14_000000_create_rockhopsoftcom_installs_tables';
+//        $dbMig = '2020_09_14_000000_create_rockhopsoftcom_installs_tables';
         $this->publishes([
 
               __DIR__ . '/Views'
                   => base_path('resources/views/vendor/rockhopsoftcom'),
 
-              __DIR__ . '/Models'
-                  => base_path('app/Models'),
-
               __DIR__ . '/Uploads' 
-                  => base_path('storage/app/up/rockhopsoftcom'),
+                  => base_path('public/rockhopsoftcom'),
               
-              __DIR__ . '/Database/' . $dbMig . '.php'
-                  => base_path('database/migrations/' . $dbMig . '.php'),
+//              __DIR__ . '/Database/' . $dbMig . '.php'
+//                  => base_path('database/migrations/' . $dbMig . '.php'),
 
               __DIR__ .'/Database/RockHopSoftComSeeder.php' 
                   => base_path('database/seeders/RockHopSoftComSeeder.php')
